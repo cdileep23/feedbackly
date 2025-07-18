69dccd5ab615937d2b09a4f0b58146ff6e466aa3
dileep <cdileepkumar22@gmail.com>
2025-07-18 23:25:35 +0530

    commit 2

--- server/routes/feedbackform.route.js
+++ server/routes/feedbackform.route.js
@@ -1,14 +1,29 @@
-import express from "express"
+import express from "express";
 import { IsAuthenticated } from "../Middleware/auth.js";
 import { createFeedbackForm, deleteFeedbackForm, getAdminFeedbackForms, getFeedbackForm, getFormAnalyticsForAdmin, toggleFeedbackFormStatus } from "../controllers/feedbackForm.controller.js";
 
-const FeedbackFormRouter=express.Router();
 
-FeedbackFormRouter.route("/create-form").post(IsAuthenticated,createFeedbackForm);//admin
-FeedbackFormRouter.route("/update-status/:formId").patch(IsAuthenticated,toggleFeedbackFormStatus);//admin
+const FeedbackFormRouter = express.Router();
 
-FeedbackFormRouter.route('/get-form/:formId').get(getFeedbackForm)//user
-FeedbackFormRouter.route("/get-all-forms").get(IsAuthenticated,getAdminFeedbackForms);
-FeedbackFormRouter.route("/:formId/analytics").get(IsAuthenticated,getFormAnalyticsForAdmin)
-FeedbackFormRouter.route('/delete-feedback/:formId').delete(IsAuthenticated,deleteFeedbackForm)
+FeedbackFormRouter.route("/create-form").post(
+  IsAuthenticated,
+  createFeedbackForm
+); //admin
+FeedbackFormRouter.route("/update-status/:formId").patch(
+  IsAuthenticated,
+  toggleFeedbackFormStatus); //admin
+
+FeedbackFormRouter.route("/get-form/:formId").get(getFeedbackForm); //user
+FeedbackFormRouter.route("/get-all-forms").get(
+  IsAuthenticated,
+  getAdminFeedbackForms
+);
+FeedbackFormRouter.route("/:formId/analytics").get(
+  IsAuthenticated,
+  getFormAnalyticsForAdmin
+);
+FeedbackFormRouter.route("/delete-feedback/:formId").delete(
+  IsAuthenticated,
+  deleteFeedbackForm
+);
 export default FeedbackFormRouter;
