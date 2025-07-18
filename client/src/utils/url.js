1a6fea43a382c0a7468dbb2fec532330e5bba8f8
dileep <cdileepkumar22@gmail.com>
2025-07-18 23:34:11 +0530

    base_url issue

--- client/src/utils/url.js
+++ client/src/utils/url.js
@@ -1,3 +1,4 @@
-export const BASE_URL = location.hostname === "localhost"
-   ? "http://localhost:4545/api/v1"
-   : "https://feedbackly-x58z.onrender.com";
+export const BASE_URL =
+  location.hostname === "localhost"
+    ? "http://localhost:4545/api/v1"
+    : "https://feedbackly-x58z.onrender.com/api/v1";
